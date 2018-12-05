a2d2df8c1e2d3ce1425f46706cf665375eb9f41c
Daniel Mayer <dmayer@lsq.com>
2018-12-05 11:48:31 -0500

    Make small changes to FileUpload interface

--- projects/rxjs-uploader/src/lib/models/file-upload.ts
+++ projects/rxjs-uploader/src/lib/models/file-upload.ts
@@ -7,37 +7,63 @@ import { HttpMethod } from './http-method';
 const uploadSuccessOrRedirectCode = /^[23]/;
 
 export interface IFileUpload {
+    /** The state and percentage of the file's upload progress. */
     progress: IProgress;
+    /** The response, if any, returned from the HTTP upload call. */
     response: Response;
+    /** The code from the HTTP response, if any (e.g. `200`). */
     responseCode: number;
+    /** The HTTP response body, if any. */
     responseBody: any;
-    url?: string;
+    /** Set to `true` the first time the file upload is executed. */
     uploadHasStarted: boolean;
-    executeStream: Observable<boolean>;
+    /** Receives a value every time the FileUpload is supposed to execute its HTTP upload. */
+    executeStream: Observable<void>;
+    /**
+     * Receives `false` when `markForRemoval()` is called, telling `Uploader` to delete it
+     * from memory and cancel the upload if one is pending.
+     */
     isMarkedForRemovalStream: Observable<boolean>;
 
+    /** Returns the value passed to `setRequestOptions()`. */
     readonly requestOptions: IUploadRequestOptions;
+    /** A unique identifier for the `FileUpload`. */
     readonly id: Symbol;
+    /** The `name` taken from `file.name`. */
     readonly name: string;
+    /** Percentage of the upload that has been completed. */
     readonly progressPercentage: number;
+    /** Boolean indicating whether an upload is in progress. */
     readonly uploading: boolean;
+    /** Boolean indicating whether the upload has completed, either successfully or not. */
     readonly uploaded: boolean;
+    /** Boolean indicating whether the upload has succeeded. */
     readonly succeeded: boolean;
+    /** Boolean indicating whether the upload has failed. */
     readonly failed: boolean;
+    /** Boolean indicating whether the upload has been marked as a failure by `Uploader`. */
     readonly rejected: boolean;
+    /** Boolean indicating whether the `FileUpload` has been marked for deletion by `Uploader`. */
     readonly isMarkedForRemoval: boolean;
 
+    /** Used by `Uploader` to mark a `FileUpload` as failed. */
     reject(errorResponse?: any): void;
+    /** Set the `IUploadRequestOptions`, used to construct the HTTP request. */
     setRequestOptions(options: IUploadRequestOptions): void;
+    /** Used by `Uploader` to construct the HTTP request. */
     createRequest(): {
         method: HttpMethod,
         url: string,
         body: FormData,
         headers?: { [key: string]: string }
     };
+    /** Resets the state of the `FileUpload`. */
     reset(): void;
+    /** Executes the file upload. */
     retry(): void;
+    /** Used by `Uploader` to mark the `FileUpload` for deletion. */
     markForRemoval(): void;
+    /** Alias for {@link IFileUpload#markForRemoval}` */
     remove(): void;
 }
 
@@ -50,7 +76,7 @@ export class FileUpload implements IFileUpload {
         method: HttpMethod.Post,
         formData: null
     };
-    private _executeSubject = new BehaviorSubject<boolean>(true);
+    private _executeSubject = new BehaviorSubject<void>(null);
     private _isMarkedForRemovalSubject = new BehaviorSubject<boolean>(false);
 
     // Public API.
@@ -61,9 +87,8 @@ export class FileUpload implements IFileUpload {
     public response: Response;
     public responseCode: number;
     public responseBody: any;
-    public url?: string;
     public uploadHasStarted = false;
-    public executeStream: Observable<boolean> = this._executeSubject.asObservable();
+    public executeStream: Observable<void> = this._executeSubject.asObservable();
     public isMarkedForRemovalStream: Observable<boolean> = this._isMarkedForRemovalSubject.asObservable();
 
     constructor(public file: File, id?: Symbol) {
@@ -158,13 +183,12 @@ export class FileUpload implements IFileUpload {
         this.response = null;
         this.responseBody = null;
         this.responseCode = null;
-        this.url = null;
         this.progress.state = ProgressState.NotStarted;
         this.progress.percent = 0;
     }
 
     public retry(): void {
-        this._executeSubject.next(true);
+        this._executeSubject.next(null);
     }
 
     public markForRemoval(): void {
